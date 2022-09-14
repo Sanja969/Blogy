3a4314c3b9b7c380c51c1deee7ebeeb1b82698fd
Sanja <sanja1mandic@gmail.com>
2022-09-14 13:32:34 +0200

    Fix linters

--- db/schema.rb
+++ db/schema.rb
@@ -12,40 +12,39 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_09_14_095836) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "comments", force: :cascade do |t|
-    t.integer "AuthorId"
-    t.integer "PostId"
-    t.text "Text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'comments', force: :cascade do |t|
+    t.integer 'AuthorId'
+    t.integer 'PostId'
+    t.text 'Text'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.integer "AuthorId"
-    t.integer "PostId"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'likes', force: :cascade do |t|
+    t.integer 'AuthorId'
+    t.integer 'PostId'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.integer "AuthorId"
-    t.text "Title"
-    t.text "Text"
-    t.integer "CommentsCounter"
-    t.integer "LikesCounter"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'posts', force: :cascade do |t|
+    t.integer 'AuthorId'
+    t.text 'Title'
+    t.text 'Text'
+    t.integer 'CommentsCounter'
+    t.integer 'LikesCounter'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "Name"
-    t.string "Photo"
-    t.text "Bio"
-    t.integer "PostCounter"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'Name'
+    t.string 'Photo'
+    t.text 'Bio'
+    t.integer 'PostCounter'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
