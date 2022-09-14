b0d6046c7b9b120d0768c7924cf300d9250b52b3
Sanja <sanja1mandic@gmail.com>
2022-09-14 13:28:53 +0200

    Change ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -1,37 +1,51 @@
-# ActiveRecord::Schema[7.0].define(version: 2022_09_14_095836) do
-#   enable_extension 'plpgsql'
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-#   create_table 'comments', force: :cascade do |t|
-#     t.integer 'AuthorId'
-#     t.integer 'PostId'
-#     t.text 'Text'
-#     t.datetime 'created_at', null: false
-#     t.datetime 'updated_at', null: false
-#   end
+ActiveRecord::Schema[7.0].define(version: 2022_09_14_095836) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
-#   create_table 'likes', force: :cascade do |t|
-#     t.integer 'AuthorId'
-#     t.integer 'PostId'
-#     t.datetime 'created_at', null: false
-#     t.datetime 'updated_at', null: false
-#   end
+  create_table "comments", force: :cascade do |t|
+    t.integer "AuthorId"
+    t.integer "PostId"
+    t.text "Text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
-#   create_table 'posts', force: :cascade do |t|
-#     t.integer 'AuthorId'
-#     t.text 'Title'
-#     t.text 'Text'
-#     t.integer 'CommentsCounter'
-#     t.integer 'LikesCounter'
-#     t.datetime 'created_at', null: false
-#     t.datetime 'updated_at', null: false
-#   end
+  create_table "likes", force: :cascade do |t|
+    t.integer "AuthorId"
+    t.integer "PostId"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
-#   create_table 'users', force: :cascade do |t|
-#     t.string 'Name'
-#     t.string 'Photo'
-#     t.text 'Bio'
-#     t.integer 'PostCounter'
-#     t.datetime 'created_at', null: false
-#     t.datetime 'updated_at', null: false
-#   end
-# end
+  create_table "posts", force: :cascade do |t|
+    t.integer "AuthorId"
+    t.text "Title"
+    t.text "Text"
+    t.integer "CommentsCounter"
+    t.integer "LikesCounter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "Name"
+    t.string "Photo"
+    t.text "Bio"
+    t.integer "PostCounter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
