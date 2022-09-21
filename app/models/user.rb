5bca67655a56591f27799530b81b9333d3b5341e
Sanja <sanja1mandic@gmail.com>
2022-09-21 09:40:28 +0200

    - Set up associations between user and other models - Add method for returning recent 3 posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy, foreign_key: 'Author_id'
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  def recent_3_posts
+    posts.last(3)
+  end
 end
