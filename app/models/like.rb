c1c5a3b3073210a8805dc6601ddfaead067463a4
Sanja <sanja1mandic@gmail.com>
2022-09-21 09:43:56 +0200

    - Set up associations between like and other models - Add method for updating number of like for liked post

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,9 @@
 class Like < ApplicationRecord
+  belongs_to :user, foreign_key: 'Author_id'
+  belongs_to :post, foreign_key: 'Post_id'
+
+  def counter
+    post.LikesCounter.nil? ? post.LikesCounter = 1 : post.LikesCounter += 1
+    post.update(LikesCounter: post.LikesCounter)
+  end
 end
