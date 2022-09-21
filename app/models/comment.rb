ff2dea617009c14041da95b2cfa0029807e7e37e
Sanja <sanja1mandic@gmail.com>
2022-09-21 09:43:25 +0200

    - Set up associations between comment and other models - Add method for updating number of comments for commented post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,9 @@
 class Comment < ApplicationRecord
+  belongs_to :user, foreign_key: 'Author_id'
+  belongs_to :post, foreign_key: 'Post_id'
+
+  def counter
+    post.CommentsCounter.nil? ? post.CommentsCounter = 1 : post.CommentsCounter += 1
+    post.update(CommentsCounter: post.CommentsCounter)
+  end
 end
