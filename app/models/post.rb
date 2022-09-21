796c20a946b0a1fa8869678bb2c29638855f74df
Sanja <sanja1mandic@gmail.com>
2022-09-21 09:42:11 +0200

    - Set up associations between post and other models - Add method for updating number of post for its author - Add method for returning recent 5 comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  belongs_to :user, foreign_key: 'Author_id'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  def counter
+    user.PostCounter.nil? ? user.PostCounter = 1 : user.PostCounter += 1
+    user.update(PostCounter: user.PostCounter)
+  end
+
+  def recent_5_comments
+    comments.last(5)
+  end
 end
