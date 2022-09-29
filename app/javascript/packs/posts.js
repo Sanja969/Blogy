855bd5923ad1586d6fc66edd01b160cedcdcecbc
Sanja <sanja1mandic@gmail.com>
2022-09-30 01:24:35 +0200

    Update javascript with functionalities for comments input size

--- app/javascript/packs/posts.js
+++ app/javascript/packs/posts.js
@@ -1,17 +1,25 @@
 const postsAllBtn = document.querySelector(".all-posts-btn")
 const postsLastBtn = document.querySelector(".last-posts-btn")
 
-window.addEventListener = ("load", () => {
-  document.querySelector(".all-posts").style.display = "none"
-  document.querySelector(".last-posts").style.display = "block"
-})
+if (document.querySelector(".all-posts")) {
+  window.addEventListener = ("load", () => {
+    document.querySelector(".all-posts").style.display = "none"
+    document.querySelector(".last-posts").style.display = "block"
+  })
+  
+  postsAllBtn.addEventListener("click", () => {
+    document.querySelector(".all-posts").style.display = "block"
+    document.querySelector(".last-posts").style.display = "none"
+  })
+  
+  postsLastBtn.addEventListener("click", () => {
+    document.querySelector(".all-posts").style.display = "none"
+    document.querySelector(".last-posts").style.display = "block"
+  })
+  
+}
+const commentInput = document.querySelector(".comment-input")
 
-postsAllBtn.addEventListener("click", () => {
-  document.querySelector(".all-posts").style.display = "block"
-  document.querySelector(".last-posts").style.display = "none"
-})
-
-postsLastBtn.addEventListener("click", () => {
-  document.querySelector(".all-posts").style.display = "none"
-  document.querySelector(".last-posts").style.display = "block"
+commentInput.addEventListener("click", () => {
+  commentInput.setAttribute('rows', 6)
 })
