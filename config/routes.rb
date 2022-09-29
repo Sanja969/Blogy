89c186f6d264ae960ede79018b9558e58045182d
Sanja <sanja1mandic@gmail.com>
2022-09-30 01:14:46 +0200

    Make routes for comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, on: :show, only: [:index, :show]
   end
+
+  resources :posts, only: [:new, :create] do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create]
+  end
 end
