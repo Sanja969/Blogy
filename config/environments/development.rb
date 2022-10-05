10d2ce5adfc16c0463d4bf4409259ea0cb4fba23
Sanja <sanja1mandic@gmail.com>
2022-10-05 20:49:04 +0200

    Change development.rb to update assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,11 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
@@ -58,6 +63,8 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+
+  config.serve_static_assets = false
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
