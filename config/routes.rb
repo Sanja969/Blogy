ef42819ebe4da8f5d06fba8c309e6ddc8e071ae7
Sanja <sanja1mandic@gmail.com>
2022-10-06 13:55:06 +0200

    Add destroy route for comments

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
   resources :posts, only: [:new, :create, :destroy] do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
     resources :likes, only: [:create]
   end
 end
