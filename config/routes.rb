fb75d4c3eb2d380e31bfbddab6cb4406c776b21e
Sanja <sanja1mandic@gmail.com>
2022-10-06 12:36:04 +0200

    Add destroy for post

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, on: :show, only: [:index, :show]
+    resources :posts, on: :show, only: [:index, :show, :destroy]
   end
 
-  resources :posts, only: [:new, :create] do
+  resources :posts, only: [:new, :create, :destroy] do
     resources :comments, only: [:create]
     resources :likes, only: [:create]
   end
